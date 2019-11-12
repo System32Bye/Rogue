0220696ce5b72fea7f8adc8c032ea00b3eb2b420
System32Bye <37011559+System32Bye@users.noreply.github.com>
2019-11-13 08:31:28 +0900

    add Enemy, Loader, MovingOvject, Player, Wall scrips
    GameManager was corrected.

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-
+public class GameManager : MonoBehaviour {
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
 
     private int level = 3;
+    private List<Enemy> enemies = new List<Enemy>();
+    private bool enemiesMoving;
 
     // Use this for initialization
     void Awake()
@@ -24,14 +27,43 @@ public class GameManager : MonoBehaviour
         InitGame();
     }
 
-    void InitGame()
-    {
+    void InitGame() {
         boardScript.SetupScene(level);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void GameOver() {
+        enabled = false;
+    }
 
+    // Update is called once per frame
+    void Update() {
+        if (playersTurn || enemiesMoving)
+            return;
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToLise(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
---
+++ Roguelike/Assets/Scripts/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int hp = 4;
+
+    public void DamageWall(int loss)
+    {
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
