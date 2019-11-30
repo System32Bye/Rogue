ceb9a8c10dd4ee78b88c65d505e8ee8eb73c0e2e
System32Bye <37011559+System32Bye@users.noreply.github.com>
2019-11-30 19:56:09 +0900

    Camera follow player
    Camera follow player

---
+++ Roguelike/Assets/Scripts/MainCamera_Moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera_Moving : MonoBehaviour {
+
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 3.5f;
+    public float offsetZ = -3f;
+
+    Vector3 cameraPosition;
+
+    void LateUpdate() {
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = cameraPosition;
+    }
+
+}
