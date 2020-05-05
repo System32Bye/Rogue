3afcd93456b866e561d79cf4116b9e7835cec418
System32Bye <37011559+System32Bye@users.noreply.github.com>
2020-05-06 06:45:29 +0900

    add weapon change
    dont working weapon change function

---
+++ Roguelike/Assets/HUD.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private GunController theGunController;
+    private Gun currentGun;
+
+    //필요하면 활성화, 아니면 비활성화
+    [SerializeField]
+    private GameObject go_BulletHUD;
+
+    //총알 갯수
+    [SerializeField]
+    private Text[] text_Bullet;
+    
+	// Update is called once per frame
+	void Update () {
+        CheckBullet();
+	}
+
+    private void CheckBullet() {
+        currentGun = theGunController.GetGun();
+        text_Bullet[0].text = currentGun.reloadBulletCount.ToString();
+        text_Bullet[1].text = currentGun.currentBulletCount.ToString();
+    }
+}
