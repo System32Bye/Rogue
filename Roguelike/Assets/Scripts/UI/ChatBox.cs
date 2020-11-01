f86a4c5b9c6f5c29246090f31dcf453edbf35296
System32Bye <37011559+System32Bye@users.noreply.github.com>
2020-11-01 22:39:42 +0900

    add ChatBox and camUI
    add ChatBox and camUI

---
+++ Roguelike/Assets/Scripts/UI/ChatBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChatBox : MonoBehaviour
+{
+    public GameObject[] Chats;
+    public bool ChatCheck = true;
+
+
+    void Update()
+    {
+        if (ChatCheck)
+        {
+            StartCoroutine(ChatCoroutine());
+        }
+    }
+
+    protected IEnumerator ChatCoroutine() {
+        ChatBoxSetup(Random.Range(0, 8));
+        yield return new WaitForSeconds(2.0f);
+        ChatCheck = true;
+    }
+
+    void ChatBoxSetup(int i)
+    {
+        var clone = Instantiate(Chats[i], new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        clone.transform.parent = transform;
+        Debug.Log("채팅");
+        Destroy(clone, 2.0f);
+        ChatCheck = false;
+    }
+}
